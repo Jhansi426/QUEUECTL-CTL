fc5e65c07dae4442a4a48d74b48c457d20393ee0
Jhansi_Paluri <jhansipaluri2211@gmail.com>
2025-11-09 22:14:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="queuectl",
     version="1.0.0",
-    # author="Jhansi Paluri",
-    # author_email="jhansipaluri2211@.com",
+    author="Jhansi Paluri",
+    author_email="jhansipaluri2211@.com",
     description="QueueCTL - A CLI-based background job queue system with workers, DLQ, and dashboard",
     long_description=open("README.md").read() if open("README.md", "r") else "",
     long_description_content_type="text/markdown",
     
-    license="MIT",
     packages=find_packages(include=["core*", "cli*", "web*"]),
     py_modules=["main"],
     include_package_data=True,
